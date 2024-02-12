cccb007ba3d6ad9d1317a48e33ca9beee355faf4
thermopidev <kilo_proven_0x@icloud.com>
2024-02-12 06:53:17 +0000

    Basic smart thermostat working OK

--- homekit_thermostat.py
+++ homekit_thermostat.py
@@ -4,7 +4,6 @@ import asyncio
 from pyhap.accessory import Accessory
 from pyhap.accessory_driver import AccessoryDriver
 from pyhap.const import CATEGORY_THERMOSTAT
-
 from enum import Enum, unique
 
 logging.basicConfig(level=logging.INFO, format="[%(module)s] %(message)s")
@@ -25,82 +24,39 @@ class HKThermostat(Accessory):
         
         # Characteristic options from services.json
         thermostat_service = self.add_preload_service('Thermostat')
-        self.current_heating_cooling_state = thermostat_service.configure_char('CurrentHeatingCoolingState') 
-        self.target_heating_cooling_state = thermostat_service.configure_char('TargetHeatingCoolingState', setter_callback=self.set_target_heating_cooling_state) 
-        self.curent_temperature = thermostat_service.configure_char('CurrentTemperature')
-        self.target_temperature = thermostat_service.configure_char('TargetTemperature', setter_callback=self.set_target_temperature)
-        self.temperature_units = thermostat_service.configure_char('TemperatureDisplayUnits')
-        
-        
-        self.target_heating_cooling_state_did_change_callback = None
-        self.target_temperature_did_change_callback = None
+        self._current_heating_cooling_state = thermostat_service.configure_char('CurrentHeatingCoolingState') 
+        self._target_heating_cooling_state = thermostat_service.configure_char('TargetHeatingCoolingState', setter_callback=self.set_target_heating_cooling_state) 
+        self._curent_temperature = thermostat_service.configure_char('CurrentTemperature')
+        self._target_temperature = thermostat_service.configure_char('TargetTemperature', setter_callback=self.set_target_temperature)
+        self._temperature_units = thermostat_service.configure_char('TemperatureDisplayUnits') # Can this be forced to only C?!
+        self._target_heating_cooling_state_did_change_callback = None
+        self._target_temperature_did_change_callback = None
+    
+    @property
+    def current_target_temperature(self):
+        """Returns the current target temperature."""
+        return self._target_temperature.get_value()
 
-    # @Accessory.run_at_interval(3)
-    # async def run(self):
-    #     self.curent_temperature.set_value(18)
-
+    @property
+    def current_heating_cooling_state(self):
+        """Returns the current heating/cooling state."""
+        current_state = self._current_heating_cooling_state.get_value()
+        return TargetHeatingCoolingState(current_state)
+    
     def set_current_temperature(self, value):
-        print(f"Update value: {value}")
-        self.curent_temperature.set_value(value)    
+        self._curent_temperature.set_value(value)    
     
     def set_target_temperature(self, value):
-        if self.target_temperature_did_change_callback is not None:
-            self.target_temperature_did_change_callback(value)
+        if self._target_temperature_did_change_callback is not None:
+            self._target_temperature_did_change_callback(value)
             
     def register_for_target_temperature_did_change_notifications(self, callback):
-        self.target_temperature_did_change_callback = callback
+        self._target_temperature_did_change_callback = callback
 
     def register_for_heating_cooling_state_did_change_notifications(self, callback):
-        self.target_heating_cooling_state_did_change_callback = callback
+        self._target_heating_cooling_state_did_change_callback = callback
 
     def set_target_heating_cooling_state(self, new_state):
-        if self.target_heating_cooling_state_did_change_callback is not None:
+        if self._target_heating_cooling_state_did_change_callback is not None:
             state_enum = TargetHeatingCoolingState(new_state)
-            self.target_heating_cooling_state_did_change_callback(state_enum)
-
-        
-# def get_accessory(driver):
-#     """Call this method to get a standalone Accessory."""
-#     return Thermostat(driver, 'MyTempSensor')
-        
-# driver = AccessoryDriver(port=51826)
-# driver.add_accessory(accessory=get_accessory(driver))
-# signal.signal(signal.SIGTERM, driver.signal_handler)
-# driver.start()
-
-
-
-class HomeKitThermostat:
-    def __init__(self, name: str):
-        self.driver = AccessoryDriver(port=51826)
-        self.thermostat = Thermostat(self.driver, name)
-        self.driver.add_accessory(accessory=self.thermostat)
-        signal.signal(signal.SIGTERM, self.driver.signal_handler)
-        
-        self.thermostat.target_heating_cooling_state_did_change_callback = self._target_heating_cooling_state_did_change
-        self.thermostat.target_temperature_did_change_callback = self._target_temperature_did_change
-        
-        self._on_target_heating_cooling_state_did_change_notification = None
-        self._on_target_temperature_did_change_notification = None
-        
-    def _target_heating_cooling_state_did_change(self, new_state):
-        state_enum = TargetHeatingCoolingState(new_state)
-        if self._on_target_heating_cooling_state_did_change_notification is not None:
-            self._on_target_heating_cooling_state_did_change_notification(state_enum)
-            
-    def _target_temperature_did_change(self, new_temperature):
-        if self._on_target_temperature_did_change_notification is not None:
-            self._on_target_temperature_did_change_notification(new_temperature)
-
-    def start(self):
-        self.driver.start()
-
-    def set_current_temperature(self, value):
-        print(f"VALUE: {value}")
-        self.thermostat.set_current_temperature(value)
-        
-    def register_for_heating_cooling_state_did_change_notifications(self, callback):
-        self._on_heating_cooling_state_did_change_callback = callback
-
-    def register_for_target_temperature_did_change_notifications(self, callback):
-        self._on_target_temperature_did_change_notification = callback
+            self._target_heating_cooling_state_did_change_callback(state_enum)
--- thermostat_integration_test.py
+++ thermostat_integration_test.py
@@ -2,16 +2,21 @@ import argparse
 import asyncio
 from utils.thermostat import Thermostat
 from utils.relay import Relay
-from homekit_thermostat import HKThermostat
+from homekit_thermostat import HKThermostat, TargetHeatingCoolingState
 import time
 from pyhap.accessory_driver import AccessoryDriver
 import signal
 from enum import Enum, unique
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 class ThermostatController:
     def __init__(self, loop):
+        self._logger = logging.getLogger(__name__)
         self.loop = loop
-        self.tasks = []  # Store references to for long running taskes
+        self.thermostat_task = None
+        self.driver_task = None
 
         # Setup thermostat
         self.relay = Relay(pin=26)
@@ -26,39 +31,59 @@ class ThermostatController:
         self.homekit_thermostat.register_for_target_temperature_did_change_notifications(self.target_temperature_did_change)
         self.driver.add_accessory(accessory=self.homekit_thermostat)
         signal.signal(signal.SIGTERM, self.driver.signal_handler)
-        
+                
     async def start_thermostat(self):
-        print("Starting thermostat...")
-        thermostat_task = asyncio.create_task(self.thermostat.start())
-        self.tasks.append(thermostat_task)
-        
-        print("Starting HomeKit integration...")
-        driver_task = asyncio.create_task(self.driver.async_start())
-        self.tasks.append(driver_task) 
+        self._logger.info("Starting HomeKit integration...")
+        self.driver_task = asyncio.create_task(self.driver.async_start())     
         
     def thermostat_temperature_did_change(self, new_temperature):
-        print(f"New temperature: {new_temperature}")
+        self._logger.info("Current temperature did change to: %s°C. Updating HomeKit.", new_temperature)
         self.homekit_thermostat.set_current_temperature(new_temperature)
-        
-    def target_temperature_did_change(self, new_temperature):
-        print(f"Target temperature did change: {new_temperature}")
-        self.thermostat.set_target_temperature(new_temperature)
     
+    def heating_cooling_state_did_change(self, new_state):
+        self._logger.info("HomeKit heating cooling state did change to: %s. Updating Thermostat.", new_state)
+        
+        # Cancel any existing thermostat task before starting a new action
+        if self.thermostat_task:
+            self.thermostat_task.cancel()
+            self.thermostat_task = None  # Clear the existing task
+            
+        # Depending on the new state, schedule the start or stop coroutine
+        if new_state in (TargetHeatingCoolingState.HEAT, TargetHeatingCoolingState.AUTO):
+            # Schedule the thermostat start coroutine without waiting for it
+            self.thermostat_task = asyncio.create_task(self.thermostat.start())
+        elif new_state in (TargetHeatingCoolingState.OFF, TargetHeatingCoolingState.COOL):
+            # Schedule the thermostat stop coroutine without waiting for it
+            self.thermostat_task = asyncio.create_task(self.thermostat.stop())
+            
+    def target_temperature_did_change(self, new_temperature):
+        self._logger.info("HomeKit target temperature did change to: %s. Updating Thermostat.", new_temperature)
+        self.thermostat.set_target_temperature(new_temperature)
+
+    
+    # TODO GET THIS TIDY!
     async def stop_thermostat(self):
         print("Stopping thermostat...")
-        for task in self.tasks:
-            task.cancel()  # Cancel each task
-            try:
-                await task  # Wait for the task to be cancelled
-            except asyncio.CancelledError:
-                pass  # Task cancellation is expected, so we can ignore this exception
-            
-    def heating_cooling_state_did_change(self, new_state):
-        if new_state == TargetHeatingCoolingState.OFF {
-            thermostat.stop()
-        }
 
-        
+        # Cancel and await the thermostat task if it exists
+        if self.thermostat_task:
+            self.thermostat_task.cancel()
+            try:
+                await self.thermostat_task
+            except asyncio.CancelledError:
+                print("Thermostat task cancelled.")
+            self.thermostat.shutdown()
+            self.thermostat_task = None  # Reset the task to None after cancellation
+
+        # Cancel and await the driver task if it exists
+        if self.driver_task:
+            self.driver_task.cancel()
+            try:
+                await self.driver_task
+            except asyncio.CancelledError:
+                print("Driver task cancelled.")
+            self.driver_task = None  # Reset the task to None after cancellation
+               
 async def main(loop):
     parser = argparse.ArgumentParser(description="Thermostat Control Script")
     parser.add_argument('--start', action='store_true', help="Start the thermostat")
@@ -72,6 +97,7 @@ async def main(loop):
         await controller.stop_thermostat()
         
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     

fd035e4c5faddc5f13250e377b4fe729fc1b2717
Andy Brown <andybrown41@icloud.com>
2024-05-02 06:47:19 +0100

    Switch to asyncio

--- utils/weather_api.py
+++ utils/weather_api.py
@@ -1,3 +1,5 @@
+import aiohttp
+import asyncio
 import requests
 import os
 from dotenv import load_dotenv
@@ -7,12 +9,17 @@ load_dotenv()
 api_key = os.getenv("WEATHER_API_KEY")
 location = os.getenv("COORDINATES")
 
-def get_temperature():
+async def get_temperature():
     url = f"https://api.tomorrow.io/v4/timelines?location={location}&fields=temperature&timesteps=current&units=metric&apikey={api_key}"
-    response = requests.get(url)
-    data = response.json()
-    temperature = data['data']['timelines'][0]['intervals'][0]['values']['temperature']
-    return temperature
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.json()
+            temperature = data['data']['timelines'][0]['intervals'][0]['values']['temperature']
+            return temperature
+        
+async def main():
+    current_temperature = await get_temperature()
+    print(f"The current temperature at {location} is {current_temperature}°C")
 
-current_temperature = get_temperature()
-print(f"The current temperature at {location} is {current_temperature}°C")
+if __name__ == "__main__":
+    asyncio.run(main())
